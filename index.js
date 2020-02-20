4fe32ed0c84f9c74ef5d23eebec2761133ab0df7
mahnueldev <emmanuelgbd3@gmail.com>
2020-02-20 07:36:01 +0100

    changed to jump.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var animation = bodymovin.loadAnimation({
+var animation = lottie.loadAnimation({
     container:document.getElementById('bm'),
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: 'data.json'
+    path: 'jump.json'
 })
